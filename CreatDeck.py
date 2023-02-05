5c2b721310b0d1224fa2be08de50fd4886a817a3
xuyili <xuyili3003@gmail.com>
2023-02-05 20:44:35 +0800

    upload my first code

---
+++ CreatDeck.py
@@ -0,0 +1,27 @@
+import os
+import random
+
+import pandas as pd
+
+path = 'english-vocabulary/'
+book = {'1':'1 初中-乱序.txt','2':'2 高中-乱序.txt','3':'3 四级-乱序.txt','4':'4 六级-乱序.txt','5':'托福.txt','6':'考研.txt','7':'SAT.txt'}
+
+def main(vac=4,num=10):
+	vac = str(input("请输入词汇表：1初中2高中3四级4六级5托福6考研7SAT:"))
+	num = int(input("请输入要选择的单词个数:"))
+	count = 0
+	dict = {}
+	with open(os.path.join(path,book[vac]), encoding='UTF-8') as file:
+		for line in file:
+			dict[count] = line.split('\t')[0]
+			count += 1
+
+	L1 = random.sample(range(len(dict)),num)
+	L2 = ''
+	for i in L1:
+		L2 = L2 + ',' +dict[i]
+
+	print('请写一篇{}个词的英文文章，包含以下单词：{}。并翻译成中文'.format(num*20,L2[1:]))
+
+if __name__ == '__main__':
+	main()
